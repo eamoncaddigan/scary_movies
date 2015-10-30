fa9bbeb454c7d620c1908171b3623a917e883a51
eamoncaddigan <eamon.caddigan@gmail.com>
2015-10-30 16:06:29 -0400

    More or less reding Walt Hickey's original analysis.

---
+++ fact_check_walt.R
@@ -0,0 +1,18 @@
+# Just redoing Walt Hickey's original analysis. 
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+budgetData <- read_excel("scary_movies.xlsx", sheet = 1) %>%
+  rename(genre = movie_genre_display_name) %>%
+  mutate(total_gross = movie_financial_summary_domestic_box_office + 
+           movie_financial_summary_international_box_office,
+         multiple = total_gross / movie_financial_summary_production_budget) %>%
+  group_by(genre) %>%
+  mutate(percent_by_genre = 100 * cume_dist(-multiple))
+
+p <- ggplot(budgetData, aes(x = multiple, y = percent_by_genre, color = genre)) + 
+  geom_line(size=2) + 
+  xlim(1, 10) + ylim(0, 80) + scale_color_brewer(type = "qual")
+print(p)
