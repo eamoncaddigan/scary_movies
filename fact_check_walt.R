8ade7f96a1548fab329ddf9b29c566c7687957ea
eamoncaddigan <eamon.caddigan@gmail.com>
2015-10-30 16:08:28 -0400

    Better column name.

--- fact_check_walt.R
+++ fact_check_walt.R
@@ -5,10 +5,11 @@ library(dplyr)
 library(ggplot2)
 
 budgetData <- read_excel("scary_movies.xlsx", sheet = 1) %>%
-  rename(genre = movie_genre_display_name) %>%
+  rename(genre = movie_genre_display_name,
+         budget = movie_financial_summary_production_budget) %>%
   mutate(total_gross = movie_financial_summary_domestic_box_office + 
            movie_financial_summary_international_box_office,
-         multiple = total_gross / movie_financial_summary_production_budget) %>%
+         multiple = total_gross / budget) %>%
   group_by(genre) %>%
   mutate(percent_by_genre = 100 * cume_dist(-multiple))
 
