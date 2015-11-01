9951f2400bb45a2f5f9a31cb0d2afcd3f6237885
eamoncaddigan <eamon.caddigan@gmail.com>
2015-10-31 20:05:27 -0400

    Expanding x limits.

--- cheap_movies.R
+++ cheap_movies.R
@@ -5,14 +5,15 @@ library(ggplot2)
 
 source("read_data.R")
 
-# Now let's look at the data for cheaper movies
+# Now let's look at the data for cheaper movies.
 budgetDataCheap <- budgetData %>%
   filter(budget < 3e6) %>%
   group_by(genre) %>%
   mutate(percent_by_genre = 100 * cume_dist(-multiple)) %>%
   ungroup()
 
+# The difference between the genres doesn't look so big now.
 p <- ggplot(budgetDataCheap, aes(x = multiple, y = percent_by_genre, color = genre)) + 
   geom_line(size=2) + 
-  xlim(1, 10) + ylim(0, 80) + scale_color_brewer(type = "qual")
+  xlim(0, 10) + ylim(0, 80) + scale_color_brewer(type = "qual")
 print(p)
