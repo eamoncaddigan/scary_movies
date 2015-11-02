d4c4905ba995e612f6517aade9c8e8de675a42a8
eamoncaddigan <eamon.caddigan@gmail.com>
2015-11-01 19:52:09 -0500

    Better column name

--- read_data.R
+++ read_data.R
@@ -1,14 +1,15 @@
 # Prep the data
 
+library(readxl)
 library(dplyr)
 library(tidyr)
 
 budgetData <- read_excel("scary_movies.xlsx", sheet = 1) %>%
   rename(genre = movie_genre_display_name,
          budget = movie_financial_summary_production_budget) %>%
-  mutate(total_gross = movie_financial_summary_domestic_box_office + 
+  mutate(total_boxoffice = movie_financial_summary_domestic_box_office + 
            movie_financial_summary_international_box_office,
-         multiple = total_gross / budget) %>%
+         multiple = total_boxoffice / budget) %>%
   group_by(genre) %>%
   mutate(percent_by_genre = 100 * cume_dist(-multiple)) %>%
   ungroup()
