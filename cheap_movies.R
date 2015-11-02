7eea4147f697e0684b360eae0829093d1de97039
eamoncaddigan <eamon.caddigan@gmail.com>
2015-11-02 15:46:10 -0500

    This is probably more representative.

--- cheap_movies.R
+++ cheap_movies.R
@@ -7,7 +7,8 @@ source("read_data.R")
 
 # Now let's look at the data for cheaper movies.
 budgetDataCheap <- budgetData %>%
-  filter(budget < 3e6) %>%
+  filter(budget >= 1e6,
+         budget <= 3e6) %>%
   group_by(genre) %>%
   mutate(percent_by_genre = 100 * cume_dist(-multiple)) %>%
   ungroup()
@@ -15,5 +16,6 @@ budgetDataCheap <- budgetData %>%
 # The difference between the genres doesn't look so big now.
 p <- ggplot(budgetDataCheap, aes(x = multiple, y = percent_by_genre, color = genre)) + 
   geom_line(size=2) + 
-  xlim(0, 10) + ylim(0, 80) + scale_color_brewer(type = "qual")
+  coord_cartesian(xlim = c(1, 10), ylim = c(0, 80)) + 
+  scale_color_brewer(type = "qual")
 print(p)
